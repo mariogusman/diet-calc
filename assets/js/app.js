63bf0687d37f7d18fbf2b89e742ddaa93b02ac6c
Mario Gusman <mariogusman@gmail.com>
2021-03-12 23:48:39 +0000

    calc update
    calc working with results printed to console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,27 @@
-function calculate(){
-    let gender = String(document.getElementById("gender").value);
-    let weight = Number(document.getElementById("weight").value);
-    let height = Number(document.getElementById("height").value);
-    let age = Number(document.getElementById("age").value);
-    let tdee = 0;
+
+
+function calcTDEE(){
+    let gender = document.getElementById("gender").value;
+    let weight = parseInt(document.getElementById("weight").value);
+    let height = parseInt(document.getElementById("height").value);
+    let age = parseInt(document.getElementById("age").value);
+    let tdee
+    console.log("gender: " + gender + weight + height + age);
+
+    if (gender == "male") {
+        tdee = (88362 + (13397 * weight) + (4799 * height) - (5677 * age))/1000;
+        console.log(tdee);
+        return (tdee);
+        
+    } else if (gender == "female") {
+        tdee = (447593 + (9247 * weight) + (3098 * height) - (4330 * age))/1000;
+        console.log(tdee);
+        return (tdee);
+    }
     
-    console.log("Gender is: " + gender);
-    console.log("Weight is: " + weight);
-    console.log("Height is: " + height);
-    console.log("Age is: " + age);
-    console.log("TDEE is: " + tdee);
+
 }
 
 // Men: 88.362 + (13.397 × weight in kg) + (4.799 × height in cm) - (5.677 × age in years)
+
 // Women: 447.593 + (9.247 × weight in kg) + (3.098 × height in cm) - (4.330 × age in years)
