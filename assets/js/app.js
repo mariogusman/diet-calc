8828a9de48d90dd20b6e617a32f6a6ee7d431308
Mario Gusman <mariogusman@gmail.com>
2021-03-14 13:32:50 +0000

    code cleanup
    updates variable names to improve readbility

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,35 +6,39 @@ let age;
 let tdee;
 let kcalLoss;
 let kcalGain;
+let activityLevel;
 
-//  Calculates TDEE based on user input
-function calcTDEE(){
+//  Calculates BMR based on user input
+function calcBMR(){
     gender = document.getElementById("gender").value;
     weight = parseInt(document.getElementById("weight").value);
     height = parseInt(document.getElementById("height").value);
     age = parseInt(document.getElementById("age").value);
+    activityLevel = document.getElementById("actLevel").value;
 
     if (gender == "male") { //if user is male, will execute male formula, update variables and call printResults function
-        tdee = (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age));
+        bmr = (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age));
 
-        kcalMaintain = tdee;
-        kcalLoss = tdee * 0.9;
-        kcalGain = tdee * 1.1;
+        dailyBMR = bmr;
+        dailyTDEE = bmr * activityLevel;
+        kcalLoss = dailyTDEE * 0.8;
+        kcalGain = dailyTDEE * 1.1;
+        console.log("BMR: " + bmr);
         printResults();
         
     } else if (gender == "female") { //if user is female, will execute female formula, update variables and call printResults function
-        tdee = (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age));
+        bmr = (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age));
 
-        kcalMaintain = tdee;
-        kcalLoss = tdee * 0.9;
-        kcalGain = tdee * 1.1;
+        dailyBMR = bmr;
+        dailyTDEE = bmr * activityLevel;
+        kcalLoss = dailyTDEE * 0.8;
+        kcalGain = dailyTDEE * 1.1;
+        console.log("BMR: " + bmr);
         printResults();
     } else {
-        alert("Please provide all the details. If you are unsure on how to respond, check our FAQ and if you encounter an issue please contact us.")
-        
+        alert("Please provide all the details. If you are unsure on how to respond, check our FAQ and if you encounter an issue please contact us.");
     }
-    
-}
+};
 
 function printResults(){ //Prints results to HTML while truncating numbers to improve readability
 
@@ -44,10 +48,16 @@ function printResults(){ //Prints results to HTML while truncating numbers to im
     results.classList.remove("d-none");
 
     // Now that div is visible, prints the results into the span tags
-    document.getElementById('dailyMaintain').innerHTML = Math.trunc(kcalMaintain);
+    document.getElementById('dailyBMR').innerHTML = Math.trunc(dailyBMR);
+    document.getElementById('dailyTDEE').innerHTML = Math.trunc(dailyTDEE);
     document.getElementById('dailyLose').innerHTML = Math.trunc(kcalLoss);
     document.getElementById('dailyGain').innerHTML = Math.trunc(kcalGain);
-}
+};
+
+function clearResults(){
+    let results = document.getElementById("results-list");
+    results.classList.add("d-none");
+};
 
 // Formula for Men: 88.362 + (13.397 × weight in kg) + (4.799 × height in cm) - (5.677 × age in years)
 // Formula for Women: 447.593 + (9.247 × weight in kg) + (3.098 × height in cm) - (4.330 × age in years)
